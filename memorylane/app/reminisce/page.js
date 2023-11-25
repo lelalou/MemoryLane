a32b1cf4d0b44a7250ac2818f0dd7227e37a2c10
Ayesha <aqaisar3@uwo.ca>
2023-11-25 11:05:53 -0500

    adding logo

--- memorylane/app/reminisce/page.js
+++ memorylane/app/reminisce/page.js
@@ -4,18 +4,16 @@ export default function Reminisce(){
         <>
         <div className="bg-white-100 min-h-screen ">
 
-            <div className="flex items-center justify-center h-screen text-stone-300">
+            <div style={{height:'100vh'}}className="flex items-center justify-center h-screen text-stone-300">
                 <button type="button" className="flex-shrink-0 w-1/4 h-40 mx-4 bg-green-900 rounded-lg border text-2xl font-bold hover:bg-lime-800">People</button>
                 <button type="button" className="flex-shrink-0 w-1/4 h-40 mx-4 bg-green-900 rounded-lg border  text-2xl font-bold hover:bg-lime-800">Places</button>
                 <button type="button" className="flex-shrink-0 w-1/4 h-40 mx-4 bg-green-900 rounded-lg border  text-2xl font-bold hover:bg-lime-800">Moments</button>
-
-
             </div>
+            <footer className="w-50 h-50 m-0 p-0">
+                <img className="w-30 h-40"src="memorylane.png"/>
+            </footer>
         
         </div>
-
-
-
         </>
     )
 }
