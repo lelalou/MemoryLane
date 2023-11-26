77a74fa54f63f082f69fd517f7abb5795752d225
Ayesha <aqaisar3@uwo.ca>
2023-11-26 08:29:04 -0500

    homepage sizing

--- memorylane/app/page.js
+++ memorylane/app/page.js
@@ -11,12 +11,12 @@ export default function Home() {
 
       </div> */}
 
-      <div className=' relative top-40 left-60 w-200 h-100 bg-lightblue'>
+      <div className=' relative top-20 left-60 w-200 h-100 bg-lightblue'>
         <img src="logo.png" className="flex justify-center" style={{ bottom: "0", width: '45%', height: '45%', objectFit: 'cover' }}/>
 
       </div>
 
-      <div  className='relative top-40 left-80 align center pt-10'>
+      {/* <div  className='relative top-40 right-10 align center pt-10'>
           <button type="button" className='mx-8 w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Remind </button>
         
       </div>
@@ -25,12 +25,28 @@ export default function Home() {
       <Link href="/reminisce">
           <button type="button" className='mx-2 w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Reminisce </button>
           </Link>
+      </div> */}
+
+<div className='px-80 pt-20 absolute top-1/2 transform -translate-y-1/2 text-center flex flex-row'>
+        <div className='mx-12 mb-4'>
+          <button type="button" className='w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md'>
+            Remind
+          </button>
+        </div>
+
+        <div className='mx-12'>
+          <Link href="/reminisce">
+            <button type="button" className='w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md'>
+              Reminisce
+            </button>
+          </Link>
+        </div>
       </div>
 
-      
-      
 
-      <div className='flex justify-center relative top-10 h-500'>
+
+
+      <div className='flex justify-center relative top-60 h-500'>
         <img src="road.png" className="left-30 " style={{  bottom: "0", width: '60%', height: '60%', objectFit: 'cover' }}/>
       </div>
        
