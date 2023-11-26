94a3e9309953961d526f7b994f7305ea993e4b7e
Ayesha <aqaisar3@uwo.ca>
2023-11-26 02:56:37 -0500

    text-to-speech

--- memorylane/app/page.js
+++ memorylane/app/page.js
@@ -5,31 +5,33 @@ import { Link } from '@chakra-ui/react'
 export default function Home() {
   return (
     <>
-      <div className='bg-green-900 min-h-screen '>
+      <div className='bg-green-900 h-screen '>
         
       {/* <div style={{backgroundImage: "url('logo.png')", backgroundSize: "cover", backgroundPosition:"center", boxShadow:"0 4px 6px rgba(80, 80, 80, 0.1)" }}className='w-3/4 h-60'>
 
       </div> */}
 
-      <div className=' relative top-40 left-80 w-200 h-100 bg-lightblue'>
-      <img src="logo.png" className="flex justify-center" style={{ bottom: "0", width: '50%', height: '50%', objectFit: 'cover' }}/>
+      <div className=' relative top-40 left-60 w-200 h-100 bg-lightblue'>
+        <img src="logo.png" className="flex justify-center" style={{ bottom: "0", width: '45%', height: '45%', objectFit: 'cover' }}/>
 
       </div>
 
-      <div className='relative top-40 py-5 px-5'>
-        <button type="button" className='w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Remind </button>
-      </div>
-
-      <div className='relative top-40 left-80 py-5 px-5'>
-        <Link href="/reminisce">
-        <button type="button" className=' w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Reminisce </button>
-        </Link>
+      <div  className='relative top-40 left-80 align center pt-10'>
+          <button type="button" className='mx-8 w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Remind </button>
         
       </div>
 
+      <div  className='relative button-70 left-90  pt-10'>
+      <Link href="/reminisce">
+          <button type="button" className='mx-2 w-40 h-20 border border-amber-100 bg-amber-100 hover:bg-amber-300 rounded-md' > Reminisce </button>
+          </Link>
+      </div>
+
       
-      <div className='flex justify-center relative bottom-20 h-500'>
-        <img src="road.png" className="left-30 " style={{  bottom: "0", width: '80%', height: '80%', objectFit: 'cover' }}/>
+      
+
+      <div className='flex justify-center relative top-10 h-500'>
+        <img src="road.png" className="left-30 " style={{  bottom: "0", width: '60%', height: '60%', objectFit: 'cover' }}/>
       </div>
        
 
---
+++ memorylane/server/listen2.py
@@ -0,0 +1,28 @@
+import io
+from google.cloud import speech
+from google.oauth2 import service_account
+
+client_file="mymemorylaneKEY.json"
+
+
+credentialss = service_account.Credentials.from_service_account_file(client_file)
+client = speech.SpeechClient(credentials=credentialss)
+
+
+#loading the audio fild
+audio_file = 'audio1.wav'
+with io.open(audio_file,'rb') as f:
+    content = f.read()
+    audio = speech.RecognitionAudio(content=content)
+
+config = speech.RecognitionConfig(
+    encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz = 48000,
+    language_code = 'en-US'
+)
+
+response = client.recognize(config=config, audio=audio)
+print(response)
+for result in response.results:
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
